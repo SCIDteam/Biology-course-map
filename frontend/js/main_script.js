ad24619bf797be5e5c72770e8bd03a82931c0f5b
Ritisha Sharma <ritishas2000@gmail.com>
2026-06-26 14:24:28 -0700

    Move index.html to root and fix paths

--- frontend/js/main_script.js
+++ frontend/js/main_script.js
@@ -17,7 +17,7 @@ window.onload = adjustSVGSize;
 // Adjust SVG size on window resize
 window.onresize = adjustSVGSize;
 
-d3.json('../data/all_courses_py.json').then(coursesData => {
+d3.json('frontend/data/all_courses_py.json').then(coursesData => {
 
     const subjects = [...new Set(coursesData.map(course => course.course_code.slice(0, 4)))];
     const themes = [...new Set(coursesData.flatMap(course => course.themes))];
